94a202f8a8d3c2c7a8dc0f3b539b06a0a819fa2b
jd1XV <112297041+jd1XV@users.noreply.github.com>
2026-05-03 02:00:38 -0400

    New phase of testing

--- src/main.c
+++ src/main.c
@@ -5,31 +5,24 @@
 
 jd_AppMainFn {
     jd_DebugPrintSysInfo();
-    jd_ErrorLogInit(jd_StrLit("error_logs/log.txt"), 128);
+    jd_ErrorLogInit(jd_StrLit("error_logs/log.txt"), 64);
     
     jd_App* app = jd_AppCreate(&(jd_AppConfig){JD_AM_RELOADABLE, jd_StrLit("jd_app_test")});
     
     jd_WindowConfig w_config = {
         .app = app,
         .title = jd_StrLit("jd_app Test"),
-        .id_str = jd_StrLit("MainWindowID"),
-        .function_name = jd_StrLit("WindowFunc"),
-        //.function_ptr = WindowFunc,
+        .id_string = jd_StrLit("main_window"),
+        .function_name = jd_StrLit("jd_app_test_window_main"),
         .titlebar_style = jd_TitleBarStyle_Right
     };
     
-#if 0
     jd_Window* main_window = jd_AppCreateWindow(&w_config);
-#endif
-    
-    w_config.id_str = jd_StrLit("win2id");
-    w_config.function_name = jd_StrLit("win2func");
-    
-    jd_Window* win2 = jd_AppCreateWindow(&w_config);
     
     while (jd_AppIsRunning(app)) {
         jd_AppPlatformUpdate(app);
     }
+    
     
     return 0;
 }
--- src/reloadable.c
+++ src/reloadable.c
@@ -4,33 +4,66 @@
 #include <jd_ui.h>
 #include <jd_icon_font.h>
 
-jd_AppWindowFunction(WindowFunc) {
-    static b32 done = false;
-    static jd_String string = {0};
-    static jd_Arena* arena = 0;
-    static jd_UIViewport* vp2 = 0;
-    while (!done) {
-        arena = jd_ArenaCreate(0, 0);
-        vp2 = jd_UIInitForWindow(window);
-        done = true;
-    }
-    
-    jd_UIBegin(vp2);
-    jd_UIFontPush(jd_StrLit("OS_BaseFontWindows"));
-    
-    jd_UIFontPop();
-    jd_UIEnd();
-    
-    jd_WindowDrawFPS(window, jd_TextOrigin_BottomLeft, (jd_V2F){0.0f, jd_WindowGetDrawSize(window).y});
-}
+static f32 dpi_scaling = 0.0f;
+static jd_Arena* perm_arena = 0;
+static jd_Arena* frame_arena = 0;
 
-jd_AppWindowFunction(win2func) {
-    static b32 init = false;
-    static jd_UIViewport* vp = 0;
-    static jd_Arena* perm_arena = 0;
-    static jd_Arena* frame_arena = 0;
+void StatsPanel(jd_Window* window) {
     static f32* frametimes = 0;
     static u32  frametime_sample_count = 16;
+    
+    if (!frametimes) {
+        frametimes = jd_ArenaAlloc(perm_arena, sizeof(f32) * frametime_sample_count);
+    }
+    
+    jd_UIRowGrowBegin(jd_StrLit("statspanel"), jd_V2F(5.0f * dpi_scaling, 5.0f * dpi_scaling), 5.0 * dpi_scaling, 0);
+    {
+        static u64 index = 0;
+        static u64 update_delay = 2;
+        frametimes[index % frametime_sample_count] = jd_WindowGetFrameTime(window);
+        index++;
+        
+        static f32 framerate = 0.0f;
+        
+        if (index % update_delay == 0) {
+            f32 accum = 0;
+            for (u64 i = 0; i < frametime_sample_count; i++) {
+                accum += frametimes[i];
+            }
+            
+            f32 avg = accum / (f32)frametime_sample_count;
+            framerate = 1000.0 / avg;
+            update_delay = jd_Max(2, framerate);
+            
+        }
+        
+        jd_String label = jd_StringPushF(frame_arena, jd_StrLit("FPS: %.0f###FPS_Readout"), framerate);
+        
+        jd_UILabel(label);
+        jd_UILabel(jd_StrLit("|###1"));
+        
+        jd_UIBoxRec* debug_box = jd_UIGetDebugBox();
+        if (debug_box) {
+            jd_UILabel(jd_StrLit("|###3"));
+            jd_String formatted_string = jd_StringPushF(frame_arena, 
+                                                        jd_StrLit("String ID: %s - RSize: X: %.2f Y: %.2f - FSize: X: %.2f Y: %.2f"),
+                                                        debug_box->string_id.mem,
+                                                        debug_box->requested_size.x,
+                                                        debug_box->requested_size.y,
+                                                        debug_box->rect.x1,
+                                                        debug_box->rect.y1);
+            
+            jd_UILabel(formatted_string);
+        }
+        
+    }
+    jd_UIRegionEnd();
+}
+
+jd_AppWindowFunction(jd_app_test_window_main) {
+    static b32 init = false;
+    static jd_UIViewport* vp = 0;
+    
     static jd_String text_edit_string = {0};
     static u32 text_edit_string_size = 1024;
     
@@ -39,155 +72,27 @@ jd_AppWindowFunction(win2func) {
     
     if (!init) {
         vp = jd_UIInitForWindow(window);
-        frame_arena = jd_ArenaCreate(0, 0);
         perm_arena = jd_ArenaCreate(0, 0);
-        text_edit_string.mem = jd_ArenaAlloc(perm_arena, sizeof(c8) * text_edit_string_size);
-        text_edit_string.count = 0;
-        text_edit_string2.mem = jd_ArenaAlloc(perm_arena, sizeof(c8) * text_edit_string_size);
-        text_edit_string.count = 0;
-        frametimes = jd_ArenaAlloc(perm_arena, sizeof(f32) * frametime_sample_count);
+        frame_arena = jd_ArenaCreate(0, 0);
         init = true;
+        text_edit_string = jd_DiskFileReadFromPath(perm_arena, jd_StrLit("assets/emoji_test.txt"), true);
     }
     
     jd_ArenaPopTo(frame_arena, 0);
     
-    jd_UIBegin(vp);
-    jd_UIFontPush(jd_StrLit("OS_BaseFontWindows"));
-    jd_UIStyle style = jd_default_style_dark;
-    jd_V4F global_bg_color = {.1, .3, .8, 1.0};
-    style.bg_color = global_bg_color;
+    dpi_scaling = jd_WindowGetDPIScale(window);
     
-    jd_UIRegionBegin(jd_StrLit("##globalparent"), &style, (jd_UISize){0}, jd_UILayout_TopToBottom, 0.0f, false);
+    jd_UIBegin(vp);
+    jd_UIFontPush(jd_OSBaseFont(), 12 * dpi_scaling);
+    jd_UIWindowRegionBegin(jd_V2F(400.0f, 400.0f), jd_UILayout_TopToBottom, 0.0f);
     {
         jd_AppDefaultTitlebar(window);
+        StatsPanel(window);
         
-        jd_UISize size = {
-            .rule = {jd_UISizeRule_Grow, jd_UISizeRule_FitChildren},
-        };
-        
-        jd_UIRegionBegin(jd_StrLit("##menuregion"), &jd_default_style_dark, size, jd_UILayout_LeftToRight, 3.0f, true);
-        {
-            static b32 show_file_menu = false;
-            jd_UISize menu_size = {
-                .rule = {jd_UISizeRule_FitChildren, jd_UISizeRule_FitChildren} 
-            };
-            
-            if (jd_UILabelButton(jd_StrLit("File")).last_active) {
-                jd_UIRegionBeginAnchored(jd_StrLit("##filemenu"), &jd_default_style_dark, jd_UIGetLastBox(), (jd_V2F){0.0, 1.0}, (jd_V2F){0}, menu_size, jd_UILayout_TopToBottom, 0.0f, 0);
-                {
-                    
-                    jd_UILabelButton(jd_StrLit("New###1"));
-                    jd_UILabelButton(jd_StrLit("Open###2"));
-                    jd_UILabelButton(jd_StrLit("Save###3"));
-                    jd_UILabelButton(jd_StrLit("Save As###4"));
-                }
-                jd_UIRegionEnd();
-            }
-            
-            
-            if (jd_UILabelButton(jd_StrLit("Edit")).last_active) {
-                jd_UIRegionBeginAnchored(jd_StrLit("##editmenu"), &jd_default_style_dark, jd_UIGetLastBox(), (jd_V2F){0.0, 1.0}, (jd_V2F){0}, menu_size, jd_UILayout_TopToBottom, 0.0f, 0);
-                {
-                    jd_UILabelButton(jd_StrLit("Assify"));
-                    jd_UILabelButton(jd_StrLit("Dick Up"));
-                    jd_UILabelButton(jd_StrLit("Enshittify"));
-                    jd_UILabelButton(jd_StrLit("Crash the program"));
-                    jd_UILabelButton(jd_StrLit("Free Transform"));
-                    jd_UILabelButton(jd_StrLit("Shuffle"));
-                    
-                }
-                jd_UIRegionEnd();
-            }
-            
-            if (jd_UILabelButton(jd_StrLit("About")).last_active) {
-                jd_UIRegionBeginAnchored(jd_StrLit("##optionsmenu"), &jd_default_style_dark, jd_UIGetLastBox(), (jd_V2F){0.0, 1.0}, (jd_V2F){0}, menu_size, jd_UILayout_TopToBottom, 0.0f, 0);
-                {
-                    
-                }
-                jd_UIRegionEnd();
-            }
-        }
-        jd_UIRegionEnd();
-        
-        jd_UIRegionBegin(jd_StrLit("##middleregion"), &style, (jd_UISize){0}, jd_UILayout_LeftToRight, 0.0f, false);
-        {
-            jd_UISize sidebar_size = {
-                .rule = {jd_UISizeRule_FitChildren, jd_UISizeRule_Grow}
-            };
-            
-            f64 dpi_scale = jd_WindowGetDPIScale(window);
-            
-            jd_UIRegionBegin(jd_StrLit("##sidebar"), &jd_default_style_dark, sidebar_size, jd_UILayout_TopToBottom, 0.0f, false);
-            {
-                jd_V2F button_size = {50.0f * dpi_scale, 50.0f * dpi_scale};
-                jd_UIFixedSizeButton(jd_StrLit(jd_FontIcon_Up), button_size, (jd_V2F){0.5, 0.5});
-                jd_UIFixedSizeButton(jd_StrLit(jd_FontIcon_Down), button_size, (jd_V2F){0.5, 0.5});
-                jd_UIFixedSizeButton(jd_StrLit(jd_FontIcon_Left), button_size, (jd_V2F){0.5, 0.5});
-                jd_UIFixedSizeButton(jd_StrLit(jd_FontIcon_Right), button_size, (jd_V2F){0.5, 0.5});
-            }
-            jd_UIRegionEnd();
-            
-            jd_UIRegionBegin(jd_StrLit("##textedittest"), &jd_default_style_dark, (jd_UISize){jd_UISizeRule_Grow, jd_UISizeRule_Grow}, jd_UILayout_TopToBottom, 0.0f, false);
-            {
-                jd_UIInputTextBox(jd_StrLit("##texteditinputbox"), &text_edit_string, text_edit_string_size, 0, (jd_UISize){jd_UISizeRule_Grow, jd_UISizeRule_Grow});
-                jd_UIInputTextBox(jd_StrLit("##texteditinputbox2"), &text_edit_string2, text_edit_string_size2, 0, (jd_UISize){jd_UISizeRule_Grow, jd_UISizeRule_Grow});
-            }
-            jd_UIRegionEnd();
-        }
-        jd_UIRegionEnd();
-        
-        jd_UIRegionBegin(jd_StrLit("##statspanel"), &jd_default_style_dark, size, jd_UILayout_LeftToRight, 3.0f, true);
-        {
-            static u64 index = 0;
-            static u64 update_delay = 200;
-            frametimes[index % frametime_sample_count] = jd_WindowGetFrameTime(window);
-            index++;
-            
-            static f32 framerate = 0.0f;
-            
-            if (index % update_delay == 0) {
-                f32 accum = 0;
-                for (u64 i = 0; i < frametime_sample_count; i++) {
-                    accum += frametimes[i];
-                }
-                
-                f32 avg = accum / (f32)frametime_sample_count;
-                framerate = 1000.0 / avg;
-                update_delay = framerate;
-                
-            }
-            
-            jd_String label = jd_StringPushF(frame_arena, jd_StrLit("FPS: %.0f###FPS_Readout"), framerate);
-            
-            jd_UILabel(label);
-        }
-        jd_UIRegionEnd();
+        jd_UIInputTextBoxMultiline(jd_StrLit("multiline_text_edit"), jd_UIMakeShape(jd_UIGrow, jd_UIGrow), &text_edit_string, jd_StrLit("This is where the emoji should go"), text_edit_string.count); 
     }
     jd_UIRegionEnd(); 
     jd_UIFontPop();
     jd_UIEnd();
     
-    // TODO: Fix me! This should go before UIEnd, but the pointers aren't living long enough
-    
-    
 }
-
-
-#if 0
-
-WindowFunc () {
-    b32 init = false;
-    jd_UIState* ui_state = 0;
-    if (!init) {
-        ui_state = jd_UIInitForWindow(window);
-    }
-    
-    while (!ui_state->closed) {
-        jd_UIPushActiveWindow(ui_state);
-        
-        jd_UIPopActiveWindow();
-        
-    }
-}
-
-#endif
